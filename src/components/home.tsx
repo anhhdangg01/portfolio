71f964513abdf6c373b7aca6b9a06643ff14a61e
anhhdangg01 <dangaq1@uci.edu>
2025-06-21 23:44:16 -0700

    Added home page and fixed more formatting

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import '../styles/home.css';
 
 function Home() {
   return (
     <div className="home">
-
+      <div className="text-section">
+        <h1>Hello there!</h1>
+        <h2>My name is Anh Dang.</h2>
+      </div>
     </div>
   )
 }
