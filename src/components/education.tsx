040a34303cbcc4695f9c7fa9d67d4dfbb2b5dfda
anhhdangg01 <dangaq1@uci.edu>
2025-06-24 20:31:21 -0700

    Added education page, adding image later

--- src/components/education.tsx
+++ src/components/education.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import education_data from '../data/education.json';
 import '../styles/education.css';
+
+const education = education_data.education;
 
 function Education() {
   return (
     <div className="education">
-
+      <h1>Education</h1>
+      {education.map((e) => {
+        return(
+          <div className="text-section">
+            <h2>{e.education}</h2>
+            <h3>{e.degree}</h3>
+            <p>{e.graduation}</p>
+          </div>
+        )
+      })}
     </div>
   )
 }
