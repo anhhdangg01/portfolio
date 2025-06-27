bd70fea9d7649d9de59fe0bef98fa438afec3cca
anhhdangg01 <dangaq1@uci.edu>
2025-06-26 18:12:57 -0700

    Further improved formatting and added about me page

--- src/components/me.tsx
+++ src/components/me.tsx
@@ -3,9 +3,18 @@ import '../styles/me.css';
 
 function Me() {
   return (
-    <div className="me">
-
+    <div className="background">
+      <div className="me">
+        <h1>About Me</h1>
+        <div className="about-me">
+          I am passionate about UI/UX design and art! I believe expressing oneself by creating interactable 
+          yet stunning interfaces for all kinds of people to enjoy is a wonderful experience. My goal is to 
+          further enhance my personal skills and knowledge in UI/UX and art, in order to fully deliver that 
+          experience to you.
+        </div>
+      </div>
     </div>
+
   )
 }
 
