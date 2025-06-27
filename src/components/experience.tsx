c3c286412c2d180282b55b950ab6da27389daa19
anhhdangg01 <dangaq1@uci.edu>
2025-06-26 19:49:18 -0700

    Added experience page

--- src/components/experience.tsx
+++ src/components/experience.tsx
@@ -1,10 +1,35 @@
 import React from 'react';
 import '../styles/experience.css';
+import experience_data from '../data/experience.json';
+
+const experiences = experience_data.experiences;
 
 function Experience() {
   return (
-    <div className="experience">
-
+    <div className="experiences">
+      <h1>Experience</h1>
+      {experiences.map((experience) => {
+        return(
+          <div className="experience">
+            <a href={experience.link}>
+              {/* <img src={require(experience.path)} /> */}
+              <img src={require('../assets/100blackmenOC_SAC_icon.png')}></img>
+              <h2>{experience.name}</h2>
+            </a>
+            
+            <div className="info">
+              <ul>
+                {experience.description.map((desc) => {
+                  return(
+                    <li>{desc}</li>
+                  )
+                })}
+              </ul>
+            </div>
+          </div>
+        )
+      })
+      }
     </div>
   )
 }
